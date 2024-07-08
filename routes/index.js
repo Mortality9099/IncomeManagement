0b8a62780ed9e0148d48481bb55352c3c72f2dbe
mehdiathari <mohandesathari@gmail.com>
2024-07-08 21:20:52 +0430

    Frontend Refactor

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const authMiddleWare = require('../middlewares/authentication');
 
-/* GET home page. */
+
 router.get('/', authMiddleWare, function(req, res, next) {
   res.redirect('/expenses');
 });
